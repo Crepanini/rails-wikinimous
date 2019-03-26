8004461c2a5a459f1e10a87e1aa38d9650634e85
Nini Zhu <pn.zhu76@gmail.com>
2019-03-26 16:52:32 +0800

    article test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.delete_all if Rails.env.development?
+
+10.times do
+  Article.create(title: Faker::TvShows::StrangerThings.character, content: Faker::TvShows::StrangerThings.quote)
+end
+
+puts "You've created #{Article.count} articles"
